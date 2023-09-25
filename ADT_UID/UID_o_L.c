b3868f999a3a3229dabcf296d8bc39ee0134f76d
Kau4r <22102606@usc.edu.ph>
2023-09-25 21:38:35 +0800

    Linked List Updated

--- ADT_UID/UID_o_L.c
+++ ADT_UID/UID_o_L.c
@@ -1,65 +1,90 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct node {
-    char data;
+    char Elem;
     struct node *link;
 } c_type, *SET;
 
-SET Difference(SET A, SET B);
-SET Union(SET A, SET B);
-SET Intersection(SET A, SET B);
-void push(char data, SET *X);
-void show(SET X);
+void insertElem(SET *, char);
+void deleteElem(SET *, char);
+bool isMember(SET, char);
+SET setUnion(SET, SET);
+SET setIntersection(SET, SET);
+SET setDifference(SET, SET);
+void displayElem(SET);
 
 int main() {
-    SET A, B;
+    SET A = NULL, B = NULL, C = NULL;
     int x;
     char stanlee[] = "FEDCBA";
     char carlo[] = "IHEB";
 
     for (x = 0; carlo[x] != '\0'; x++) {
-        push(carlo[x], &A);
+        insertElem(&A, carlo[x]);
     }
 
     for (x = 0; stanlee[x] != '\0'; x++) {
-        push(stanlee[x], &B);
+        insertElem(&B, stanlee[x]);
     }
 
-    show(A);
-    show(B);
+    displayElem(A);
+    displayElem(B);
 
-    SET C = Intersection(A, B);
-    show(C);
+    C = setIntersection(A, B);
+
+    displayElem(C);
 
     return 0;
 }
 
-void push(char data, SET *X) {
+void insertElem(SET *X, char Elem) {
     SET new = (SET)malloc(sizeof(struct node));
     if (new != NULL) {
-        new->data = data;
-        new->link = (*X);
-        (*X) = new;
+        SET *trav;
+        for (trav = X; *trav != NULL && (*trav)->Elem < Elem;
+             trav = &(*trav)->link) {
+        }
+        new->Elem = Elem;
+        new->link = (*trav);
+        (*trav) = new;
     }
 }
 
-SET Difference(SET A, SET B) {
+void deleteElem(SET *Y, char Elem) {
+    SET *trav, temp;
+    for (trav = Y; *trav != NULL && (*trav)->Elem != Elem;
+         trav = &(*trav)->link) {
+    }
+    if (*trav != NULL) {
+        temp = *trav;
+        *trav = temp->link;
+        free(temp);
+    }
+}
+
+bool isMember(SET A, char Elem) {
+    for (; A != NULL && A->Elem != Elem; A = A->link) {
+    }
+    return A != NULL;
+}
+
+SET setDifference(SET A, SET B) {
     SET C = NULL;
     SET *C_end = &C;
 
     while (A != NULL) {
 
-        if (B == NULL || A->data < B->data) {
+        if (B == NULL || A->Elem < B->Elem) {
             *C_end = (SET)malloc(sizeof(c_type));
             if (C_end != NULL) {
-                (*C_end)->data = A->data;
+                (*C_end)->Elem = A->Elem;
                 C_end = &(*C_end)->link;
             }
             A = A->link;
         } else {
-            if (A->data == B->data) {
+            if (A->Elem == B->Elem) {
                 A = A->link;
             }
             B = B->link;
@@ -70,7 +95,7 @@ SET Difference(SET A, SET B) {
     return C;
 }
 
-SET Union(SET A, SET B) {
+SET setUnion(SET A, SET B) {
     SET C = NULL;
     SET *C_end = &C;
 
@@ -78,14 +103,14 @@ SET Union(SET A, SET B) {
         *C_end = (SET)malloc(sizeof(c_type));
         if (C_end != NULL) {
 
-            if (A->data <= B->data) {
-                (*C_end)->data = A->data;
-                if (A->data == B->data) {
+            if (A->Elem <= B->Elem) {
+                (*C_end)->Elem = A->Elem;
+                if (A->Elem == B->Elem) {
                     B = B->link;
                 }
                 A = A->link;
             } else {
-                (*C_end)->data = B->data;
+                (*C_end)->Elem = B->Elem;
                 B = B->link;
             }
             C_end = &(*C_end)->link;
@@ -99,7 +124,7 @@ SET Union(SET A, SET B) {
     while (A != NULL) {
         *C_end = (SET)malloc(sizeof(c_type));
         if (C_end != NULL) {
-            (*C_end)->data = A->data;
+            (*C_end)->Elem = A->Elem;
             A = A->link;
             C_end = &(*C_end)->link;
         }
@@ -108,21 +133,21 @@ SET Union(SET A, SET B) {
     return C;
 }
 
-SET Intersection(SET A, SET B) {
+SET setIntersection(SET A, SET B) {
     SET C = NULL;
     SET *C_end = &C;
 
     while (A != NULL && B != NULL) {
 
-        if (A->data == B->data) {
+        if (A->Elem == B->Elem) {
             *C_end = (SET)malloc(sizeof(c_type));
             if (C_end != NULL) {
-                (*C_end)->data = A->data;
+                (*C_end)->Elem = A->Elem;
                 C_end = &(*C_end)->link;
             }
             A = A->link;
             B = B->link;
-        } else if (A->data > B->data) {
+        } else if (A->Elem > B->Elem) {
             B = B->link;
         } else {
             A = A->link;
@@ -133,10 +158,10 @@ SET Intersection(SET A, SET B) {
     return C;
 }
 
-void show(SET X) {
-    printf("\n\nSHOW\n");
+void displayElem(SET X) {
+    printf("\Elements\n");
     for (; X != NULL; X = X->link) {
-        printf("%c", X->data);
+        printf("%c", X->Elem);
     }
     printf("\n");
 }
